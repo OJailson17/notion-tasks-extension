6078356a854e10c5fde9db640517698c7a251ec4
OJailson17 <JaylsonO17@gmail.com>
2022-10-22 00:09:17 -0300

    finish project

--- src/lib/axios.ts
+++ src/lib/axios.ts
@@ -1,8 +1,7 @@
 import axios from 'axios';
 
 const prodUrl = `${import.meta.env.VITE_PROD_URL}`;
-const devUrl = `${import.meta.env.VITE_DEV_URL}`;
 
 export const api = axios.create({
-	baseURL: devUrl,
+	baseURL: prodUrl,
 });
